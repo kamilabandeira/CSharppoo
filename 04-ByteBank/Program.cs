79dedd9d764b7e20fb5f0ca7bf2e8a09aee4c5eb
Kamila Bandeira <kamila_bandeira@hotmail.com>
2021-08-17 21:43:57 -0300

    continuação

---
+++ 04-ByteBank/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace _04_ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente contaDoBruno = new ContaCorrente();
+
+            contaDoBruno.titular = "Bruno";
+
+            Console.WriteLine(contaDoBruno.saldo);
+
+            bool resultadoSaque = contaDoBruno.Sacar(500);
+
+            Console.WriteLine(resultadoSaque);
+            Console.WriteLine(contaDoBruno.saldo);
+
+            contaDoBruno.Depositar(500);
+            Console.WriteLine(contaDoBruno.saldo);
+
+
+
+            ContaCorrente contaDaGabriela = new ContaCorrente();
+
+
+            contaDaGabriela.titular = "Gabriela";
+
+            Console.WriteLine("Saldo do Bruno: " + contaDoBruno.saldo);
+            Console.WriteLine("Saldo do Gabriela: " + contaDaGabriela.saldo);
+
+            bool resultadoTransferencia = contaDoBruno.Trasferir(200, contaDaGabriela);
+
+            Console.WriteLine("Saldo do Bruno: " + contaDoBruno.saldo);
+            Console.WriteLine("Saldo do Gabriela: " + contaDaGabriela.saldo);
+
+            Console.WriteLine("Resultado trasferêsncia: " + resultadoTransferencia);
+
+            contaDaGabriela.Trasferir(100, contaDoBruno);
+
+            Console.WriteLine("Saldo do Bruno: " + contaDoBruno.saldo);
+            Console.WriteLine("Saldo do Gabriela: " + contaDaGabriela.saldo);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 05-ByteBank/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace _05_ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Cliente gabriela = new Cliente();
+
+            //gabriela.nome = "Gabriela";
+            //gabriela.cpf = "Desenvolvedora C#";
+            //gabriela.profissao = "434.562.878-20";
+
+            ContaCorrente conta = new ContaCorrente();
+
+            //conta.titular = gabriela;
+            conta.titular = new Cliente();
+            conta.titular.nome = "Gabriela Costa";
+            conta.titular.cpf = "434.562.878-20";
+            conta.titular.profissao = "Desenvolvedora C#";
+
+            conta.saldo = 500;
+            conta.agencia = 563;
+            conta.numero = 5634527;
+
+            //conta.titular.nome = "Gabriela Costa";
+
+            //Console.WriteLine(gabriela.nome);
+            Console.WriteLine(conta.titular.nome);
+            Console.WriteLine(conta.titular.cpf);
+            Console.WriteLine(conta.titular.profissao);
+
+            Console.ReadLine();
+
+        }
+    }
+}
