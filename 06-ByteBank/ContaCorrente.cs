5d1183e7e2ff060eba4cdac2e275775cde1a7b27
Kamila Bandeira <kamila_bandeira@hotmail.com>
2021-08-20 11:23:52 -0300

    atribuindo getters/setters

---
+++ 06-ByteBank/ContaCorrente.cs
@@ -0,0 +1,62 @@
+﻿namespace _06_ByteBank
+{
+    public class ContaCorrente
+    {
+        
+        public Cliente Titular { get; set; }
+
+        public int Agencia { get; set; }
+
+        public int numero { get; set; }
+                      
+        private double _saldo = 100;
+
+        public double Saldo
+        {
+            get 
+            {
+                return _saldo;
+            }
+            set 
+            {
+                if (value < 0) 
+                {
+                    return;
+                }
+
+                _saldo = value;
+            }
+        }
+        
+       
+        public bool Sacar(double valor) 
+        {
+            if(_saldo < valor) 
+            {
+                return false;
+            }
+            
+            _saldo -= valor;
+            return true;
+            
+        }
+
+        public void Depositar(double valor) 
+        {
+            _saldo += valor;
+        }
+
+        public bool Trasferir(double valor, ContaCorrente contaDestino) 
+        {
+            if(_saldo < valor) 
+            {
+                return false;
+            }
+            
+            _saldo -= valor;
+            contaDestino.Depositar(valor);
+            return true;
+            
+        }
+    }
+}
